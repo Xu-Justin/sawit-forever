fbb8bd33cd6ba48d1ae688b8d18188fa88495cb5
William Justin <williamjustinxu@gmail.com>
2026-02-27 21:03:49 +0700

    initialize sawit-client

---
+++ sawit-client/src-tauri/gen/android/app/src/main/java/site/justinxu/sawit/MainActivity.kt
@@ -0,0 +1,11 @@
+package site.justinxu.sawit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
